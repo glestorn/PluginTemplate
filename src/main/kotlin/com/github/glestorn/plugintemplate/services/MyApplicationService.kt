4794906327aab41c8acd0823048ebe22e6e6e323
GitHub Action <action@github.com>
2021-01-28 10:01:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/glestorn/plugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/glestorn/plugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.glestorn.plugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.glestorn.plugintemplate.MyBundle
 
 class MyApplicationService {
 
